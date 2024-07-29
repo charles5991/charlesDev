2d9d9aea8a19e17e306062e05ff64ea79b3f9bd3
charles5991 <charleschan5991@gmail.com>
2024-07-29 15:33:07 +0800

    Update page.tsx

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -2,6 +2,7 @@ import { Jobs } from "@/app/work/components/jobs"
 import { WorkShell } from "@/app/work/components/work-shell"
 import { Heading } from "@/components/heading"
 import { defaultVariants } from "@/components/motion.variants"
+import { Text } from "@/components/text"
 
 export const metadata = {
    title: "Work - Charles",
@@ -23,25 +24,29 @@ export default async function WorkPage() {
                Learn more about my work experience, my focus areas, and what
                I&apos;m currently working on.
             </p>
-            <a href="/Charles_Chan_-_frontend_oct.pdf" download>
-               <button className="flex w-[150px] cursor-pointer justify-between rounded-full bg-transparent px-3 py-2 tracking-wider text-white shadow-xl duration-500 hover:scale-105 hover:bg-gray-900 hover:ring-1">
-                  Resume
+            <button className="flex items-center justify-center gap-2 border border-opacity-[0.03] bg-neutral-200/50 px-3 filter backdrop-blur-xl dark:bg-neutral-900/75">
+               <a
+                  href="/Charles_Chan_-_frontend_oct.pdf"
+                  download
+                  className="flex items-center gap-2"
+               >
+                  <Text>Resume</Text>
                   <svg
                      xmlns="http://www.w3.org/2000/svg"
                      fill="none"
                      viewBox="0 0 24 24"
-                     stroke-width="2"
+                     strokeWidth="2"
                      stroke="currentColor"
                      className="h-5 w-5 animate-bounce"
                   >
                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
                      ></path>
                   </svg>
-               </button>
-            </a>
+               </a>
+            </button>
             <Jobs />
          </WorkShell>
       </section>
