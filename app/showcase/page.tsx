cf67ba72ab92fe0fe1e73ff2e7256cf0768f0838
charles5991 <charleschan5991@gmail.com>
2024-07-29 15:12:18 +0800

    upload cv

--- app/showcase/page.tsx
+++ app/showcase/page.tsx
@@ -1,90 +1,262 @@
+"use client"
+
 import Image from "next/image"
 
 const showcase = () => {
    return (
       <section>
          <div className="mx-auto max-w-screen-xl px-2 py-4 sm:py-4 lg:px-6">
-            <div className="grid h-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-5">
+            <div className="my-4 grid h-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-1">
                <div className="col-span-2 flex h-auto flex-col sm:col-span-1 md:col-span-2 md:h-full">
                   <a
-                     href=""
+                     href="https://charles-infinitownmock.netlify.app/"
                      className="group relative flex flex-grow flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
+                     target="_blank"
+                     rel="noopener noreferrer"
                   >
                      <Image
                         fill={true}
-                        src="https://images.unsplash.com/photo-1510812431401-41d2bd2722f3?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        src="/assets/town.png"
                         alt=""
                         className="absolute inset-0  object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
                      />
                      <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
-                     <h3 className="xs:text-xl absolute left-0 top-0 z-10 p-4 text-2xl font-medium text-white md:text-xl">
-                        Wines
+                     <h3 className="text-md absolute left-0 top-0 z-10 p-4 font-medium text-white">
+                        Infinitown (Mock)
+                     </h3>
+                  </a>
+               </div>
+            </div>
+            <div className="my-4 grid h-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
+               <div className="col-span-2 flex h-auto flex-col sm:col-span-1 md:col-span-2 md:h-full">
+                  <a
+                     href="https://charles-r3-f-orbit-test.vercel.app/"
+                     className="group relative flex flex-grow flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                     <Image
+                        fill={true}
+                        src="/assets/mall.png"
+                        alt=""
+                        className="absolute inset-0  object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
+                     />
+                     <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
+                     <h3 className="text-md absolute left-0 top-0 z-10 p-4 font-medium text-white">
+                        Mall ThreeJs
+                     </h3>
+                  </a>
+               </div>
+               <div className="col-span-2 flex h-auto flex-col sm:col-span-1 md:col-span-2 md:h-full">
+                  <a
+                     href="https://weather-pallarax.vercel.app/"
+                     className="group relative flex flex-grow flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                     <Image
+                        fill={true}
+                        src="/assets/weather.png"
+                        alt=""
+                        className="absolute inset-0 object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
+                     />
+                     <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
+                     <h3 className="text-md absolute left-0 top-0 z-10 p-4 font-medium text-white">
+                        Weather App
+                     </h3>
+                  </a>
+               </div>
+            </div>
+            <div className="grid h-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-5">
+               <div className="col-span-2 flex h-auto flex-col sm:col-span-1 md:col-span-2 md:h-full">
+                  <a
+                     href="https://charles-atmosphere.vercel.app/"
+                     className="group relative flex flex-grow flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                     <Image
+                        fill={true}
+                        src="/assets/plane.png"
+                        alt=""
+                        className="absolute inset-0  object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
+                     />
+                     <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
+                     <h3 className="text-md absolute left-0 top-0 z-10 p-4 font-medium text-white">
+                        Charles Atmosphere
                      </h3>
                   </a>
                </div>
                <div className="col-span-2 sm:col-span-1 md:col-span-2">
                   <a
-                     href=""
+                     href="https://apple-clone-parallax.vercel.app/"
                      className="group relative mb-4 flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
+                     target="_blank"
+                     rel="noopener noreferrer"
                   >
                      <Image
                         fill={true}
-                        src="https://images.unsplash.com/photo-1504675099198-7023dd85f5a3?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        src="/assets/iphone.png"
                         alt=""
                         className="absolute inset-0  object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
                      />
                      <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
-                     <h3 className="xs:text-xl absolute left-0 top-0 z-10 p-4 text-2xl font-medium text-white md:text-xl">
-                        Gin
-                     </h3>
+                     <p className=" text-md absolute left-0 top-0 z-10 p-4 font-medium text-white">
+                        Apple clone
+                     </p>
                   </a>
                   <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 lg:grid-cols-2">
                      <a
-                        href=""
+                        href="https://blackhole-phi.vercel.app/"
                         className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
+                        target="_blank"
+                        rel="noopener noreferrer"
                      >
                         <Image
                            fill={true}
-                           src="https://images.unsplash.com/photo-1571104508999-893933ded431?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                           src="/assets/blackhole.png"
                            alt=""
                            className="absolute inset-0  object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
                         />
                         <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
-                        <h3 className="xs:text-xl absolute left-0 top-0 z-10 p-4 text-2xl font-medium text-white md:text-xl">
-                           Whiskey
+                        <h3 className="  text-md absolute left-0 top-0 z-10 p-4 font-medium text-white  ">
+                           Blackhole ThreeJs
                         </h3>
                      </a>
                      <a
-                        href=""
+                        href="https://emperor-royal.vercel.app/"
                         className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
+                        target="_blank"
+                        rel="noopener noreferrer"
                      >
                         <Image
                            fill={true}
-                           src="https://images.unsplash.com/photo-1626897505254-e0f811aa9bf7?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                           src="/assets/emperor.png"
                            alt=""
                            className="absolute inset-0  object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
                         />
                         <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
-                        <h3 className="xs:text-xl absolute left-0 top-0 z-10 p-4 text-2xl font-medium text-white md:text-xl">
-                           Vodka
+                        <h3 className="  text-md absolute left-0 top-0 z-10 p-4 font-medium text-white  ">
+                           Emperor Royal SG Website
                         </h3>
                      </a>
                   </div>
                </div>
                <div className="col-span-2 flex h-auto flex-col  sm:col-span-1 md:col-span-1 md:h-full">
                   <a
-                     href=""
+                     href="https://cleverly-web.vercel.app/projects"
                      className="group relative flex flex-grow flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
+                     target="_blank"
+                     rel="noopener noreferrer"
                   >
                      <Image
                         fill={true}
-                        src="https://images.unsplash.com/photo-1693680501357-a342180f1946?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        src="/assets/roas.png"
                         alt=""
                         className="absolute inset-0 h-full w-full object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
                      />
                      <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
-                     <h3 className="xs:text-xl absolute left-0 top-0 z-10 p-4 text-2xl font-medium text-white md:text-xl">
-                        Brandy
+                     <h3 className="  text-md absolute left-0 top-0 z-10 p-4 font-medium text-white  ">
+                        Cleverly ROAS
+                     </h3>
+                  </a>
+               </div>
+            </div>
+            <div className="my-4 grid h-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-8">
+               <div className="col-span-2 flex h-auto flex-col sm:col-span-1 md:col-span-2 md:h-full">
+                  <a
+                     href="https://ocean-r3-fiber.vercel.app/"
+                     className="group relative flex flex-grow flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                     <Image
+                        fill={true}
+                        src="/assets/ocean.png"
+                        alt=""
+                        className="absolute inset-0  object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
+                     />
+                     <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
+                     <h3 className="text-md absolute left-0 top-0 z-10 p-4 font-medium text-white">
+                        Ocean ThreeJs
+                     </h3>
+                  </a>
+               </div>
+               <div className="col-span-2 flex h-auto flex-col sm:col-span-1 md:col-span-2 md:h-full">
+                  <a
+                     href="https://childhood-gameboy.vercel.app/"
+                     className="group relative flex flex-grow flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                     <Image
+                        fill={true}
+                        src="/assets/gameboy.png"
+                        alt=""
+                        className="absolute inset-0  object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
+                     />
+                     <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
+                     <h3 className="text-md absolute left-0 top-0 z-10 p-4 font-medium text-white">
+                        Gameboy ThreeJs
+                     </h3>
+                  </a>
+               </div>
+               <div className="col-span-2 flex h-auto flex-col sm:col-span-1 md:col-span-2 md:h-full">
+                  <a
+                     href="https://sourceflow-test.vercel.app/"
+                     className="group relative flex flex-grow flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                     <Image
+                        fill={true}
+                        src="/assets/sourceflow.png"
+                        alt=""
+                        className="absolute inset-0  object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
+                     />
+                     <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
+                     <h3 className="text-md absolute left-0 top-0 z-10 p-4 font-medium text-white">
+                        Sourceflow (freelance)
+                     </h3>
+                  </a>
+               </div>
+               <div className="col-span-2 flex h-auto flex-col sm:col-span-1 md:col-span-2 md:h-full">
+                  <a
+                     href="https://austinsourceflow.vercel.app/"
+                     className="group relative flex flex-grow flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                     <Image
+                        fill={true}
+                        src="/assets/austin.png"
+                        alt=""
+                        className="absolute inset-0  object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
+                     />
+                     <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
+                     <h3 className="text-md absolute left-0 top-0 z-10 p-4 font-medium text-white">
+                        Austin website (freelance)
+                     </h3>
+                  </a>
+               </div>
+            </div>
+            <div className="my-4 grid h-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-1">
+               <div className="col-span-2 flex h-auto flex-col sm:col-span-1 md:col-span-2 md:h-full">
+                  <a
+                     href="https://888casino-v2.vercel.app/"
+                     className="group relative flex flex-grow flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                     <Image
+                        fill={true}
+                        src="/assets/casino.png"
+                        alt=""
+                        className="absolute inset-0  object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
+                     />
+                     <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
+                     <h3 className="text-md absolute left-0 top-0 z-10 p-4 font-medium text-white">
+                        Solana Casino App
                      </h3>
                   </a>
                </div>
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -23,6 +23,25 @@ export default async function WorkPage() {
                Learn more about my work experience, my focus areas, and what
                I&apos;m currently working on.
             </p>
+            <a href="/Charles_Chan_-_frontend_oct.pdf" download>
+               <button className="flex w-[150px] cursor-pointer justify-between rounded-full bg-transparent px-3 py-2 tracking-wider text-white shadow-xl duration-500 hover:scale-105 hover:bg-gray-900 hover:ring-1">
+                  Resume
+                  <svg
+                     xmlns="http://www.w3.org/2000/svg"
+                     fill="none"
+                     viewBox="0 0 24 24"
+                     stroke-width="2"
+                     stroke="currentColor"
+                     className="h-5 w-5 animate-bounce"
+                  >
+                     <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
+                     ></path>
+                  </svg>
+               </button>
+            </a>
             <Jobs />
          </WorkShell>
       </section>
